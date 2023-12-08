235ab6c071a4e25bf8d50f3d10c9e80d715e7b6a
Garrett Park <72951078+g2101@users.noreply.github.com>
2023-12-08 15:15:32 -0500

    fixed the js problem

--- script.js
+++ script.js
@@ -1,47 +1,37 @@
 function caesarCipher(input, shiftAmount) {
-  var outputString = ""
+  var outputString = "";
 
-  for (var i = 0; i < input.length; i++) {
-    var letter = input[i]
+  for (var i = 0; i < input.length; i++) { // iterate through entire word
+    var letter = input[i];
+    var ascii = input.charCodeAt(i);
 
-      var ascii = input.charCodeAt(i)
+    // if shiftAmount of over 26 is entered, take modulo down to manageable number
+    shiftAmount %= 26; 
 
-      if (ascii >= 65 && ascii <= 90) {
-        // for all uppercase letters
-        letter = String.fromCharCode(((ascii - 65 + shiftAmount) % 26) + 65)
-      }
-      else if (ascii >= 97 && ascii <= 122) {
-        // for all lowercase letters
-        letter = String.fromCharCode(((ascii - 97 + (shiftAmount % 26))) + 97)
-      }
-      /* else if (ascii >= 48 && ascii <= 57) {
-        // for numbers
-        letter = String.fromCharCode(((ascii - 97 + (shiftAmount % 9))) + 48)
-      } */
-      
-      outputString += letter 
-
-    if (shiftAmount > 0){
-    var output = document.getElementById("caesarEncryptOutput")
-    output.value =
-      "The encoded string using the Caesar cipher is:\n" + outputString
+    if (ascii >= 65 && ascii <= 90) {
+      // for all uppercase letters
+      letter = String.fromCharCode(((ascii - 65 + shiftAmount + 26) % 26) + 65);
+    } else if (ascii >= 97 && ascii <= 122) {
+      // for all lowercase letters
+      letter = String.fromCharCode(((ascii - 97 + shiftAmount + 26) % 26) + 97);
     }
-    else {
-      var output = document.getElementById("caesarDecryptOutput")
-    output.value =
-      "The decoded string using the Caesar cipher is:\n" + outputString
-    }
+
+    outputString += letter;
   }
+
+  return outputString;
 }
 
 function encryptCaesar() {
-  const input = document.getElementById("caesarEncryptInput").value
-  const shiftAmount = parseInt(document.getElementById("shiftEncryptAmount").value)
-  caesarCipher(input, shiftAmount)
+  const input = document.getElementById("caesarEncryptInput").value;
+  const shiftAmount = parseInt(document.getElementById("shiftEncryptAmount").value);
+  const output = document.getElementById("caesarEncryptOutput");
+  output.value = "The encoded string using the Caesar cipher is:\n" + caesarCipher(input, shiftAmount);
 }
 
 function decryptCaesar() {
-  const input = document.getElementById("caesarDecryptInput").value
-  const shiftAmount = parseInt(document.getElementById("shiftDecryptAmount").value)
-  caesarCipher(input, -shiftAmount)
+  const input = document.getElementById("caesarDecryptInput").value;
+  const shiftAmount = parseInt(document.getElementById("shiftDecryptAmount").value);
+  const output = document.getElementById("caesarDecryptOutput");
+  output.value = "The decoded string using the Caesar cipher is:\n" + caesarCipher(input, -shiftAmount);
 }
