01a6f7f87eadce9c026bf7a1a02a706102a9eba4
Garrett Park <garrett.park.06@icloud.com>
2023-12-11 10:08:18 -0500

    still working on monday

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Caesar Cipher Code
 function caesarCipher(input, shiftAmount) {
   var outputString = "";
 
@@ -9,29 +10,63 @@ function caesarCipher(input, shiftAmount) {
     shiftAmount %= 26; 
 
     if (ascii >= 65 && ascii <= 90) {
-      // for all uppercase letters
+      // for all uppercase letters (ASCII 65-90)
       letter = String.fromCharCode(((ascii - 65 + shiftAmount + 26) % 26) + 65);
     } else if (ascii >= 97 && ascii <= 122) {
-      // for all lowercase letters
+      // for all lowercase letters (ASCII 97-122)
       letter = String.fromCharCode(((ascii - 97 + shiftAmount + 26) % 26) + 97);
     }
 
+    // append ciphered letter to output string
     outputString += letter;
   }
 
   return outputString;
 }
 
-function encryptCaesar() {
+function encryptCaesar() { // function to return encrypted string to output
   const input = document.getElementById("caesarEncryptInput").value;
   const shiftAmount = parseInt(document.getElementById("shiftEncryptAmount").value);
   const output = document.getElementById("caesarEncryptOutput");
-  output.value = "The encoded string using the Caesar cipher is:\n" + caesarCipher(input, shiftAmount);
+  output.value = "The encoded string using the Caesar cipher is:\n" + caesarCipher(input, shiftAmount); // use a positive shift amount to encrypt cipher text
 }
 
-function decryptCaesar() {
+function decryptCaesar() { // function to return decrypted string to output
   const input = document.getElementById("caesarDecryptInput").value;
   const shiftAmount = parseInt(document.getElementById("shiftDecryptAmount").value);
   const output = document.getElementById("caesarDecryptOutput");
-  output.value = "The decoded string using the Caesar cipher is:\n" + caesarCipher(input, -shiftAmount);
+  output.value = "The decoded string using the Caesar cipher is:\n" + caesarCipher(input, -shiftAmount); // use a negative shift amount to decrypt cipher text
+}
+
+// Vigenere Cipher Code
+function vigenereCipher(input, cipher) {
+	// remove spaces from front and end of input and cipher
+	input = input.trim();
+	cipher = cipher.trim();
+
+	// make the cipher match the length of the input by looping the cipher text over
+	if (!(cipher.length == input.length)) {
+		for (let index = 0; cipher.length < input.length; index++) {
+				cipher += cipher.substring(index, index + 1); 
+		}
+		console.log("Input: " + input + " Length: " + input.length)
+		console.log("Constructed Cipher: " + cipher + " Length: " + cipher.length)
+	}
+
+	// encrypt the text using the vigenere cipher 
+	for (let index = 0; index < input.length; index++) {
+		let x = (str[index].charCodeAt(0) + key[index].charCodeAt(0)) % 26; // needs to be adjusted
+	}
+
+}
+
+function encryptVigenere() {
+	const input = document.getElementById("vigenereEncryptInput").value.toString();
+	const cipher = document.getElementById("vigenereKeywordEncrypt").value.toString();
+	const output = document.getElementById("vigenereEncryptOutput");
+	output.value = "The encoded string using the Vigenere cipher is:\n" + vigenereCipher(input, cipher);
+
+}
+
+function decryptVigenere() {
 }
